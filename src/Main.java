44b8a693a72c8027fc23108f5d21b572219701e5
mbrentrowe <m.brent.rowe@gmail.com>
2018-07-28 00:03:48 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        int wholeNumber = 123;
+        System.out.println("The variable's type is integer. Its value is " + wholeNumber);
+    }
+}
